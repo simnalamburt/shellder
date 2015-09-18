f0a8a2a77c357cc7edb191f809b83067219a1d1c
WBTMagnum <magnum@wiener-blut.at>
2015-09-19 01:26:54 +0200

    * display hostname for remote connections per default * added configuration option 'theme_hide_hostname' to disable display of hostname

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -9,6 +9,7 @@
 
 ## Set this options in your config.fish (if you want to :])
 # set -g theme_display_user yes
+# set -g theme_hide_hostname yes
 # set -g default_user your_normal_user
 
 
@@ -97,9 +98,27 @@ end
 function prompt_user -d "Display current user if different from $default_user"
   if [ "$theme_display_user" = "yes" ]
     if [ "$USER" != "$default_user" -o -n "$SSH_CLIENT" ]
-      set USER_PROMPT (whoami)@(hostname)
+      set USER (whoami)
+      get_hostname
+      if [ $HOSTNAME_PROMPT ]
+        set USER_PROMPT $USER@$HOSTNAME_PROMPT
+      else
+        set USER_PROMPT $USER
+      end
       prompt_segment black yellow $USER_PROMPT
     end
+  else
+    get_hostname
+    if [ $HOSTNAME_PROMPT ]
+      prompt_segment black yellow $HOSTNAME_PROMPT
+    end
+  end
+end
+
+function get_hostname -d "Set current hostname to prompt variable $HOSTNAME_PROMPT if connected via SSH"
+  set -g HOSTNAME_PROMPT ""
+  if [ "$theme_hide_hostname" != "yes" -a -n "$SSH_CLIENT" ]
+    set -g HOSTNAME_PROMPT (hostname)
   end
 end
 
