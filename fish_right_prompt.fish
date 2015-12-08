9da2a7e2efb94ffc0c5887e0fe5ede9ba7dcef7a
olbrew <olbrew@users.noreply.github.com>
2015-12-08 18:40:16 +0100

    match coulours to default solarized dark theme

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -6,21 +6,21 @@ set right_segment_separator \uE0B2
 function prompt_vi_mode -d 'vi mode status indicator'
     switch $fish_bind_mode
         case default
-            set_color green
-            echo -n "$right_segment_separator"
-            prompt_segment green black "N"
-        case insert
             set_color blue
             echo -n "$right_segment_separator"
-            prompt_segment blue black "I"
+            prompt_segment blue black "N"
+        case insert
+            set_color green
+            echo -n "$right_segment_separator"
+            prompt_segment green black "I"
         case visual
             set_color red
             echo -n "$right_segment_separator"
             prompt_segment red black "V"
         case replace-one
-            set_color yellow
+            set_color green
             echo -n "$right_segment_separator"
-            prompt_segment yellow black "R"
+            prompt_segment green black "R"
     end
     if [ -n $current_bg ]
         set_color -b normal
